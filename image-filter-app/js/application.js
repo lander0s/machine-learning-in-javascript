a835627b5204622816fff6a6cff59f7b9e8a41fc
David Landeros <dh.landeros08@gmail.com>
2019-06-11 18:03:10 -0700

    implement application flow (for the moment skipping the neural network work)

--- image-filter-app/js/application.js
+++ image-filter-app/js/application.js
@@ -85,16 +85,140 @@ define("DragImageElement", ["require", "exports"], function (require, exports) {
     }());
     exports.DragImageElement = DragImageElement;
 });
-define("Application", ["require", "exports", "DragImageElement"], function (require, exports, DragImageElement_1) {
+define("Pixel", ["require", "exports"], function (require, exports) {
     "use strict";
     exports.__esModule = true;
+    var Pixel = (function () {
+        function Pixel(rgba) {
+            this.r = rgba[0];
+            this.g = rgba[1];
+            this.b = rgba[2];
+            this.a = rgba[3];
+        }
+        return Pixel;
+    }());
+    exports.Pixel = Pixel;
+});
+define("RawImage", ["require", "exports", "Pixel"], function (require, exports, Pixel_1) {
+    "use strict";
+    exports.__esModule = true;
+    var RawImage = (function () {
+        function RawImage() {
+            this.pixels = [];
+            this.width = 0;
+            this.height = 0;
+            this.canvas = document.createElement('canvas');
+            this.updateCanvasContext();
+        }
+        RawImage.prototype.updateCanvasContext = function () {
+            this.canvas.width = this.width;
+            this.canvas.height = this.height;
+            this.context = this.canvas.getContext('2d');
+        };
+        RawImage.prototype.fromPixelArray = function (image, width, height) {
+            this.width = width;
+            this.height = height;
+            this.pixels = image;
+            this.updateCanvasContext();
+        };
+        RawImage.prototype.fromImage = function (image) {
+            this.width = image.width;
+            this.height = image.height;
+            this.updateCanvasContext();
+            this.context.drawImage(image, 0, 0, this.width, this.height);
+            var imageData = this.context.getImageData(0, 0, this.width, this.height);
+            this.pixels = [];
+            for (var j = 0; j < this.height; j++) {
+                for (var i = 0; i < this.width; i++) {
+                    var rgba = [];
+                    var pixelIndex = (j * this.width + i) * 4;
+                    rgba[0] = imageData.data[pixelIndex + 0];
+                    rgba[1] = imageData.data[pixelIndex + 1];
+                    rgba[2] = imageData.data[pixelIndex + 2];
+                    rgba[3] = imageData.data[pixelIndex + 3];
+                    var pixel = new Pixel_1.Pixel(rgba);
+                    this.pixels.push(pixel);
+                }
+            }
+        };
+        RawImage.prototype.invertColors = function () {
+            for (var i = 0; i < this.pixels.length; i++) {
+                this.pixels[i].r = 255 - this.pixels[i].r;
+                this.pixels[i].g = 255 - this.pixels[i].g;
+                this.pixels[i].b = 255 - this.pixels[i].b;
+            }
+        };
+        RawImage.prototype.toDataUrl = function () {
+            var imageData = this.context.getImageData(0, 0, this.width, this.height);
+            for (var i = 0; i < this.pixels.length; i++) {
+                var pixelOffset = i * 4;
+                imageData.data[pixelOffset + 0] = this.pixels[i].r;
+                imageData.data[pixelOffset + 1] = this.pixels[i].g;
+                imageData.data[pixelOffset + 2] = this.pixels[i].b;
+                imageData.data[pixelOffset + 3] = this.pixels[i].a;
+            }
+            this.context.putImageData(imageData, 0, 0);
+            return this.canvas.toDataURL();
+        };
+        return RawImage;
+    }());
+    exports.RawImage = RawImage;
+});
+define("Application", ["require", "exports", "DragImageElement", "RawImage"], function (require, exports, DragImageElement_1, RawImage_1) {
+    "use strict";
+    exports.__esModule = true;
+    var ImageRol;
+    (function (ImageRol) {
+        ImageRol[ImageRol["Example"] = 0] = "Example";
+        ImageRol[ImageRol["Filtered"] = 1] = "Filtered";
+        ImageRol[ImageRol["Challenge"] = 2] = "Challenge";
+    })(ImageRol || (ImageRol = {}));
     var Application = (function () {
         function Application() {
+            this.exampleRawImage = null;
+            this.filterRawImage = null;
+            this.challengeRawImage = null;
         }
         Application.prototype.main = function () {
+            var _this = this;
             this.exampleDragImage = new DragImageElement_1.DragImageElement('#example-image', 'original image goes here');
             this.filterDragImage = new DragImageElement_1.DragImageElement('#example-image-with-filter', 'filtered image goes here');
             this.challengeDragImage = new DragImageElement_1.DragImageElement('#challenge-image', 'a complete different image goes here');
+            this.exampleDragImage.onload = function (args) { return _this.onImageLoaded(args, ImageRol.Example); };
+            this.filterDragImage.onload = function (args) { return _this.onImageLoaded(args, ImageRol.Filtered); };
+            this.challengeDragImage.onload = function (args) { return _this.onImageLoaded(args, ImageRol.Challenge); };
+        };
+        Application.prototype.onImageLoaded = function (img, rol) {
+            switch (rol) {
+                case ImageRol.Example: {
+                    this.exampleRawImage = new RawImage_1.RawImage();
+                    this.exampleRawImage.fromImage(img);
+                    break;
+                }
+                case ImageRol.Filtered: {
+                    this.filterRawImage = new RawImage_1.RawImage();
+                    this.filterRawImage.fromImage(img);
+                    break;
+                }
+                case ImageRol.Challenge: {
+                    this.challengeRawImage = new RawImage_1.RawImage();
+                    this.challengeRawImage.fromImage(img);
+                    break;
+                }
+            }
+            if (this.isReady()) {
+                this.challengeRawImage.invertColors();
+                var imageAsUrl = this.challengeRawImage.toDataUrl();
+                var resultElem = document.querySelector('#ann-result');
+                resultElem.style.backgroundImage = "url(" + imageAsUrl + ")";
+                resultElem.style.backgroundSize = '100% 100%';
+                resultElem.className += " ann-revealed-result";
+            }
+        };
+        Application.prototype.isReady = function () {
+            return this.exampleRawImage
+                && this.filterRawImage
+                && this.challengeRawImage;
         };
         return Application;
     }());
--- image-filter-app/ts/Application.ts
+++ image-filter-app/ts/Application.ts
@@ -1,14 +1,68 @@
 import { DragImageElement } from "./DragImageElement"
+import { RawImage } from "./RawImage"
+
+enum ImageRol {
+    Example,
+    Filtered,
+    Challenge
+}
 
 export class Application {
 
-    private exampleDragImage : DragImageElement;
-    private filterDragImage : DragImageElement;
+    private exampleDragImage   : DragImageElement;
+    private filterDragImage    : DragImageElement;
     private challengeDragImage : DragImageElement;
+    private exampleRawImage   : RawImage = null;
+    private filterRawImage    : RawImage = null;
+    private challengeRawImage : RawImage = null;
 
-    main() {
+    public main() {
         this.exampleDragImage = new DragImageElement('#example-image', 'original image goes here');
         this.filterDragImage = new DragImageElement('#example-image-with-filter', 'filtered image goes here');
         this.challengeDragImage = new DragImageElement('#challenge-image', 'a complete different image goes here');
+
+        this.exampleDragImage.onload = (args : HTMLImageElement) => this.onImageLoaded(args, ImageRol.Example);
+        this.filterDragImage.onload = (args : HTMLImageElement) => this.onImageLoaded(args, ImageRol.Filtered);
+        this.challengeDragImage.onload = (args : HTMLImageElement) => this.onImageLoaded(args, ImageRol.Challenge);
+    }
+
+    private onImageLoaded(img : HTMLImageElement, rol : ImageRol) {
+        switch(rol) {
+            case ImageRol.Example : {
+                this.exampleRawImage = new RawImage();
+                this.exampleRawImage.fromImage(img);
+                break;
+            }
+            case ImageRol.Filtered : {
+                this.filterRawImage = new RawImage();
+                this.filterRawImage.fromImage(img);
+                break;
+            }
+            case ImageRol.Challenge : {
+                this.challengeRawImage = new RawImage();
+                this.challengeRawImage.fromImage(img);
+                break;
+            }
+        }
+
+        if(this.isReady()) {
+            /**
+             * Learning process and challenge solving go here!
+             * Learning process and challenge solving go here!
+             * Learning process and challenge solving go here!
+             * Learning process and challenge solving go here!
+             */
+            let imageAsUrl = this.challengeRawImage.toDataUrl();
+            let resultElem : HTMLElement = document.querySelector('#ann-result');
+            resultElem.style.backgroundImage = `url(${imageAsUrl})`;
+            resultElem.style.backgroundSize = '100% 100%';
+            resultElem.className += " ann-revealed-result";
+        }
+    }
+
+    private isReady() {
+        return this.exampleRawImage 
+            && this.filterRawImage
+            && this.challengeRawImage;
     }
 }
