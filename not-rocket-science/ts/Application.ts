8a542a6b1325f4079f39ca0611d5f841d4753095
David Landeros <dh.landeros08@gmail.com>
2019-06-17 11:48:16 -0700

    Update Application.ts ( use Learner.ts )

--- not-rocket-science/ts/Application.ts
+++ not-rocket-science/ts/Application.ts
@@ -1,19 +1,23 @@
 import { Simulator } from './Simulator'
 import { Renderer } from './Renderer'
+import { Learner } from './Learner'
 
 export class Application {
 
     private simulator : Simulator;
     private renderer  : Renderer;
+    private learner   : Learner;
 
     constructor() {
         this.simulator = new Simulator();
         this.renderer = new Renderer('#render-surface', this.simulator);
+        this.learner = new Learner(this.simulator);
     }
 
     public init() {
         this.renderer.init();
         this.simulator.init();
+        this.learner.init();
         let rocket = this.simulator.addRocket();
         rocket.setDesiredThrusterIntensityFactor(0.0);
         rocket.setDesiredThrusterAngleFactor(0.5);
@@ -24,6 +28,7 @@ export class Application {
 
     public update() {
         this.simulator.update();
+        this.learner.update();
         this.renderer.draw();
     }
 }
