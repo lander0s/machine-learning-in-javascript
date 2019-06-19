8e0ca3933351c176a38b311f13379a5f400904a7
David Landeros <dh.landeros08@gmail.com>
2019-06-19 11:13:08 -0700

    minor refactor of Leaderboard, now it uses Array of Genomes instead of Array of any

--- not-rocket-science/ts/Leaderboard.ts
+++ not-rocket-science/ts/Leaderboard.ts
@@ -2,7 +2,7 @@ import { Genome } from './Genome'
 
 export class Leaderboard {
 
-    private leadGenomes : Array<any>
+    private leadGenomes : Array<Genome>
 
     constructor() {
 
@@ -10,15 +10,11 @@ export class Leaderboard {
 
     public registerGenome(newGenome : Genome) : void {
         this.removeFromLeaderboard(newGenome);
-        this.leadGenomes.push({
-            id : newGenome.getId(),
-            fitness : newGenome.getFitness(),
-            jsonObj : newGenome.toJson()
-        });
-        this.leadGenomes.sort((a:any, b:any) => {
-            if( a.fitness < b.fitness)
+        this.leadGenomes.push(newGenome);
+        this.leadGenomes.sort((a:Genome, b:Genome) => {
+            if( a.getFitness() < b.getFitness())
                 return 1;
-            if( a.fitness > b.fitness)
+            if( a.getFitness() > b.getFitness())
                 return -1;
             return 0;
         });
@@ -29,7 +25,7 @@ export class Leaderboard {
 
     private removeFromLeaderboard(genome:Genome) : void {
         for(let i = 0; i < this.leadGenomes.length; i++) {
-            if(this.leadGenomes[i].id == genome.getId()) {
+            if(this.leadGenomes[i].getId() == genome.getId()) {
                 this.leadGenomes.splice(i,1);
                 break;
             }
@@ -47,22 +43,28 @@ export class Leaderboard {
         let html = '<br> Leaderboard <hr> <table>'
             html += '<tr><td>Genome</td><td>Fitness</td></tr>'
         for(let i = 0; i < this.leadGenomes.length; i++) {
-            html += `<tr><td>${this.leadGenomes[i].id}</td>`
-            html += `<td>${this.leadGenomes[i].fitness|0}</td></tr>`;
+            html += `<tr><td>${this.leadGenomes[i].getId()}</td>`
+            html += `<td>${this.leadGenomes[i].getFitness()|0}</td></tr>`;
         }
         html += '</table>'
         document.querySelector('#leaderboard-div').innerHTML = html;
     }
 
     private loadFromLocalStorage() : void {
+        this.leadGenomes = [];
         if(localStorage.getItem('leaderboard') != null) {
-            this.leadGenomes = JSON.parse(localStorage.getItem('leaderboard'));
-        } else {
-            this.leadGenomes = [];
+            let genomesAsJsonObjects : Array<any> = JSON.parse(localStorage.getItem('leaderboard'));
+            genomesAsJsonObjects.forEach((genomeJsonObj)=>{
+                this.leadGenomes.push(Genome.fromJson(genomeJsonObj));
+            });
         }
     }
 
     private saveToLocalStorage() : void {
-        localStorage.setItem('leaderboard', JSON.stringify(this.leadGenomes));
+        let genomesAsJsonObjects : Array<any> = [];
+        this.leadGenomes.forEach((genome:Genome) => {
+            genomesAsJsonObjects.push(genome.toJson());
+        });
+        localStorage.setItem('leaderboard', JSON.stringify(genomesAsJsonObjects));
     }
 }
