072eac12f522b4b048cc1fe84c0ec7ebc73c9a95
David Landeros <dh.landeros08@gmail.com>
2019-06-20 15:11:11 -0700

    add basic ui

--- tic-tac-toe/ts/Application.ts
+++ tic-tac-toe/ts/Application.ts
@@ -2,6 +2,13 @@
 export class Application {
 
     public main() {
-        console.log('hello world');
+        for(let i = 1; i <= 9; i++) {
+            document.querySelector(`#cell${i}`)
+                .addEventListener('click', () => this.onCellClicked(i));
+        }
+    }
+
+    public onCellClicked(cellNo:number) {
+        console.log(`cell ${cellNo} clicked`);
     }
 }
