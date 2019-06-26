2612f2d8cb749189a71da70aa29782fb9a0d775e
David Landeros <dh.landeros08@gmail.com>
2019-06-25 17:17:25 -0700

    fix build

--- tic-tac-toe/ts/AiPlayer/implementations/decisionTree/DecisionTreePlayer.ts
+++ tic-tac-toe/ts/AiPlayer/implementations/decisionTree/DecisionTreePlayer.ts
@@ -11,7 +11,7 @@ export class DecisionTreePlayer extends AiPlayer {
     }
 
     public train() : void {
-        this.decisionTree.build2();
+        this.decisionTree.build();
     }
 
     public play(board: TicTacToe.Players[], turn : TicTacToe.Players, callback:Function) : void {
