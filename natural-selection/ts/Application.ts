105844dd27239f096fcb1e36b05b5b3b02972d73
David Landeros <dh.landeros08@gmail.com>
2019-07-01 14:00:23 -0700

    update simulation for each frame

--- natural-selection/ts/Application.ts
+++ natural-selection/ts/Application.ts
@@ -4,21 +4,14 @@ import { Renderer } from './Renderer'
 export class Application {
     private mRenderer  : Renderer;
     private mSimulator : Simulator;
-    private mTimestamp : number;
 
     public main() : void {
         this.mSimulator = new Simulator();
         this.mRenderer = new Renderer(this.mSimulator);
-        this.mTimestamp = 0;
     }
 
     public update() : void {
-        let elapsedTime = new Date().getTime() - this.mTimestamp;
-        if(elapsedTime > 1000) {
-            this.mSimulator.update();    
-            this.mTimestamp = new Date().getTime();
-        }
-
+        this.mSimulator.update();
         this.mRenderer.draw();
     }
 
