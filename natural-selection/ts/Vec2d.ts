7810096bddc099cac4b267ab3f94512ad1dd784e
David Landeros <dh.landeros08@gmail.com>
2019-07-05 17:08:33 -0700

    add missing features to Vec2d

--- natural-selection/ts/Vec2d.ts
+++ natural-selection/ts/Vec2d.ts
@@ -1,46 +1,64 @@
+
 export class Vec2d {
     
-    x : number = 0;
-    y : number = 0;
+    public static Up    : Vec2d = new Vec2d(0, 1);
+    public static Down  : Vec2d = new Vec2d(0, -1);
+    public static Right : Vec2d = new Vec2d(1, 0);
+    public static Left  : Vec2d = new Vec2d(-1, 0);
+
+    public x : number = 0;
+    public y : number = 0;
     
     constructor(x : number, y : number) {
         this.x = x;
         this.y = y;
     }
     
-    add(v : Vec2d) : Vec2d {        
+    public add(v : Vec2d) : Vec2d {        
         return new Vec2d(this.x + v.x, this.y + v.y);
     }
     
     
-    copy() : Vec2d {        
+    public copy() : Vec2d {        
         return new Vec2d(this.x, this.y);
     }
     
     
-    distance(v : Vec2d) : number {
+    public distance(v : Vec2d) : number {
         return (v.subtract(this)).length();
     }
     
-    multiply(scalar : number) : Vec2d {
+    public multiply(scalar : number) : Vec2d {
         return new Vec2d(this.x * scalar, this.y * scalar);
     }
     
-    length() :number {
+    public length() :number {
         return Math.sqrt(this.x * this.x + this.y * this.y);
     }
+
+    public lengthSQ() :number {
+        return (this.x * this.x + this.y * this.y);
+    }
     
-    normalize() : Vec2d {
+    public normalize() : Vec2d {
         let len = this.length();
 
         return new Vec2d(this.x / len, this.y / len);
     }
 
-    subtract(v : Vec2d) : Vec2d {        
+    public subtract(v : Vec2d) : Vec2d {        
         return new Vec2d(this.x - v.x, this.y - v.y);
     }
 
-    toString() : string {
+    public toString() : string {
         return "(" + this.x + ", " + this.y + ")";
+    }
+
+    public getAngle(other : Vec2d) : number {
+        return Math.acos(other.dot(this));
+    }
+
+    public dot(other : Vec2d) : number {
+        return this.x * other.x + this.y * other.y;
     }
 }
